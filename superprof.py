cbe229cf01c1d52d6f105e884da5085fcd65f93c
GX Oliveira <99613544+gxdeoliveira@users.noreply.github.com>
2022-02-14 14:51:03 -0300

    Update superprof.py

--- superprof.py
+++ superprof.py
@@ -45,39 +45,4 @@ while True:
    
 print(ext_data)
 print(bio_data)
-'''
-bio_data = []
-
-page = 0
-
-# Parsing the HTML with Beautiful Soup
-
-headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36", 'accept': 'text/html; charset=utf-8'}
-while True:
-
-    page += 1
-    print(page)
-    url = f'https://www.superprof.com/s/,United-States,,,1.html?n={page}'
-    site = requests.get(url, headers=headers)  
-    soup = BeautifulSoup(site.text, 'html.parser')
-    print(soup)
-    break
-    school = 'learn'
-    school_final = school.lower()
-    # Finding all the results in professor's bio
-    link_to_profile = soup.find_all('a')
-    print(f' a bio eh esta cesar: {link_to_profile}')
-    for professors in profile:
-        professor_name = soup.find('div', {'class':'prenom'}).text
-        bios = soup.find('p', {'class':'AnnonceContent_text'}).text
-
-        print(f' checking professor name {professor_name}')
-        if school_final in bios:
-            bio_data.append(name.capitalize())
-            print(f'The professor found was: {professor_name}')
-
-            if next_page is None:
-               	break
-print(bio_data)
-      '''      
 
